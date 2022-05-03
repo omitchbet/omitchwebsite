ba4a9e2ecce03717d06e8dd6e4704fffb6d9e796
Chuksremi15 <chuksremi15@gmail.com>
2022-05-03 16:28:56 +0100

    implement new design

--- src/components/nav/WebpageNav.js
+++ src/components/nav/WebpageNav.js
@@ -1,19 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import SwipeableDrawer from "@material-ui/core/Drawer";
+import { AiOutlineClose } from "react-icons/ai";
+import { HiMenuAlt1 } from "react-icons/hi";
 
 export const WebpageNav = () => {
+  const [sideNav, setsideNav] = useState(false);
   return (
-    <div className="flex  flex-row  fixed z-30 px-4 md:px-8 py-4 w-full bg-opacity-5 backdrop-blur-sm">
-      <div className="flex-none">
-        <img className="h-8" src="/img/nav/logo.svg" />
-      </div>
-      <div className="ml-auto flex gap-3 md:gap-5">
-        <div className="bg-blueDark text-center text-white border  border-borderGray px-3 md:px-5 py-1 rounded-lg text-sm">
-          Join our community
+    <>
+      <div className="hidden lg:flex bg-[#0F0631] md:flex-row z-30 px-4 md:px-8 py-3 md:py-4 w-full  ">
+        <div className="flex-none">
+          <img className="h-8" src="/img/nav/logo.svg" />
         </div>
-        <div className="bg-blueDark text-center text-white border border-borderGray px-3 md:px-5 py-1 rounded-lg text-sm">
-          connect wallet
+        <div className="ml-auto flex gap-3 md:gap-5">
+          <div className=" text-center text-white  px-3 md:px-5 py-1 rounded-lg text-sm">
+            Gitbook
+          </div>
+          <div className=" text-center text-white  px-3 md:px-5 py-1 rounded-lg text-sm">
+            Our Community
+          </div>
+          <div className=" text-center text-white  px-3 md:px-5 py-1 rounded-lg text-sm">
+            Stake
+          </div>
+          <div className="bg-blueDark text-center text-white border border-borderGray px-3 md:px-5 py-1 rounded-lg text-sm ">
+            Launch App
+          </div>
         </div>
       </div>
-    </div>
+      <div className="grid grid-cols-8  fixed z-20 px-4 py-4 w-full  lg:hidden">
+        <div>
+          <div
+            className="text-2xl cursor-pointer text-white lg:hidden pt-1 pr-2"
+            onClick={() => setsideNav(!sideNav)}
+          >
+            <HiMenuAlt1 className="cursor-pointer" />
+          </div>
+          <SwipeableDrawer
+            anchor={"left"}
+            open={sideNav}
+            onClose={() => setsideNav(false)}
+            onOpen={() => setsideNav(true)}
+          >
+            <div className="bg-[#0F0631] h-screen w-screen text-lightGray  relative">
+              <div
+                onClick={() => setsideNav(!sideNav)}
+                className="ml-auto  text-2xl text-white absolute right-3 top-3 cursor-pointer"
+              >
+                <AiOutlineClose />
+              </div>
+              <div className="h-px mt-14 bg-lightGray">
+                <div className="mx-auto flex flex-col gap-2 pt-10">
+                  <div className=" text-center text-white  px-3 md:px-5 py-1 rounded-lg text-sm">
+                    Gitbook
+                  </div>
+                  <div className=" text-center text-white  px-3 md:px-5 py-1 rounded-lg text-sm">
+                    Our Community
+                  </div>
+                  <div className=" text-center text-white  px-3 md:px-5 py-1 rounded-lg text-sm">
+                    Stake
+                  </div>
+                  <div className="bg-blueDark w-32 mx-auto md:w-auto text-center text-white border border-borderGray px-3 md:px-5 py-1 rounded-lg text-sm ">
+                    Launch App
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwipeableDrawer>
+        </div>
+        <div className="col-span-7 ">
+          <div className="ml-auto w-12 z-40">
+            <img src="/img/nav/logo.svg" className="h-8" />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,43 +16,47 @@ const Home = () => {
 
   return (
     <div className="overflow-x-hidden">
-      <div className="bg-dark ">
+      <div className="bg-[#0F0631] relative">
         <WebpageNav />
-        <div className="relative grid md:grid-cols-2 pt-8">
-          <img
-            data-aos="fade-right"
-            alt=""
-            src="/img/showcase/showcaseimg.svg"
-            className="absolute md:h-96 lg:h-144"
-          ></img>
-          <div></div>
+        <div className="relative grid md:grid-rows-1 grid-cols-1 md:grid-cols-2 md:grid-rows-0 pt-8 container mx-auto px-8">
           <div
             data-aos="fade-zoom-in"
             data-aos-easing="ease-in-back"
             data-aos-delay="300"
             data-aos-offset="0"
-            className="mt-80 md:mt-32  lg:mt-48 px-2"
+            className="mt-4 md:mt-16 px-2 justify-self-center row-start-2 md:row-start-1"
           >
-            <div className="text-white text-[48px] text-center md:text-left  md:text-[58px] lg:text-[78px] leading-10  font-[900] uppercase font-heading tracking-wide">
-              Allow your
-            </div>
-            <div className="text-white text-[80px] text-center md:text-left   md:text-[90px] lg:text-[123px] leading-10 my-6  md:my-8 lg:my-14 font-[900] uppercase font-heading tracking-wide">
-              Passion
-            </div>
-            <div className="text-white text-[48px] text-center md:text-left  md:text-[58px] lg:text-[78px] leading-10 font-[900] uppercase font-heading tracking-wide">
-              Reward You
-            </div>
-            <p className="text-white text-center md:text-left  md:text-lg lg:text-xl tracking-wide mt-8">
-              Play and get paid on the best decentralised crypto betting
-              platform.
+            <h1 className="text-white text-4xl text-center md:text-left  md:text-5xl lg:text-6xl font-[900] uppercase font-heading max-w-sm tracking-wider">
+              The future of web 3 gaming
+            </h1>
+            <p className="text-white text-center md:text-left md:text-lg mt-2 max-w-sm md:ml-8 ">
+              OMITCH is building a decentralized gaming system owned by the
+              community where the community play with a pool of asset
             </p>
           </div>
+          <img
+            data-aos="fade-left"
+            alt=""
+            src="/img/showcase/showcaseimg.svg"
+            className="object-contain lg:h-[450px] justify-self-end row-start-1 md:row-start-1"
+          ></img>
         </div>
+        <img
+          alt=""
+          src="/img/home/sidevector.svg"
+          className="absolute right-0 top-0"
+        ></img>
 
-        <div className=" mt-16 md:mt-32 relative container max-w-screen-md mx-auto ">
-          <div className="border text-center uppercase text-white w-80 mx-auto rounded-md font-heading font-bold py-1 text-sm tracking-wider">
-            About Omitch
-          </div>
+        <img
+          alt=""
+          src="/img/home/downvector.svg"
+          className="absolute h-[600px] top-[1000px]"
+        ></img>
+
+        <div className=" mt-16 md:mt-16 relative container max-w-screen-md mx-auto ">
+          <h2 className="border text-center uppercase text-white w-80 mx-auto rounded-md font-heading font-bold py-1 text-sm tracking-wider">
+            why choose omitchbet
+          </h2>
           <p className="text-sm text-white text-center mt-5 px-8">
             Omitchbet solves the age long and current problems in the TradBet
             systems. It adopts the modern decentralised wallets tech to solve
@@ -65,77 +69,104 @@ const Home = () => {
           </p>
         </div>
 
-        <div className="mt-32 relative container mx-auto max-w-screen-lg grid grid-cols-2 px-8 overflow-x-hidden">
-          <div className="self-center">
-            <div className="uppercase text-white mr-auto font-heading font-bold text-xl tracking-wider">
-              HOW TO EARN
+        <div className="mt-24 relative container mx-auto px-8">
+          <h2 className="uppercase text-[#F8CD0D] font-heading font-bold text-2xl md:text-3xl tracking-wider text-center">
+            How to participate
+          </h2>
+          <div
+            className="grid md:grid-cols-2 gap-x-16 gap-y-8 mx-auto mt-8"
+            data-aos="fade-up"
+          >
+            <div className="bg-[#B080FF] text-white p-4 rounded-xl w-[300px] md:w-[350px]  md:justify-self-end">
+              <div className="grid grid-cols-2">
+                <h4 className="text-dark uppercase text-2xl font-bold font-heading w-24 leading-6 mt-2">
+                  Play to earn
+                </h4>
+                <img
+                  src="/img/icons/gamepad.svg"
+                  className="object-contain justify-self-end h-12"
+                ></img>
+              </div>
+              <p className="text-sm w-48 mt-32">
+                Play your favourite casino games and get rewarded. Enjoy playing
+                alone and with friends with a verity of games
+              </p>
+              <div className="bg-[#2663FF] w-[80px] text-center rounded text-sm py-1 mt-8">
+                Play
+              </div>
             </div>
-            <p className="text-sm text-white text-left max-w-lg mt-1">
-              Earning on a betting platform couldn't have been easier than this.
-              All you have to do is to connect your wallet, select any game of
-              your choice from the different interesting games we have in our
-              platform and then play.
-            </p>
-          </div>
-          <div className="justify-self-end " data-aos="fade-left">
-            <img alt="" className="object-cover" src="/img/home/dice.png"></img>
-          </div>
-        </div>
-        <div className="mt-32 relative container max-w-screen-lg mx-auto grid grid-cols-2 px-8 overflow-x-hidden">
-          <div className="justify-self-start" data-aos="fade-right">
-            <img
-              alt=""
-              className="object-cover"
-              src="/img/home/coins.png"
-            ></img>
-          </div>
-          <div className="self-center">
-            <div className="uppercase text-white text-right font-heading font-bold text-xl tracking-wider max-w-lg">
-              THE CHOICE IS YOURS
+            <div className="bg-[#8AA9F8] text-white p-4 rounded-xl w-[300px] md:w-[350px] md:justify-self-start">
+              <div className="grid grid-cols-2">
+                <h4 className="text-dark uppercase text-2xl font-bold font-heading w-24 leading-6 mt-2">
+                  Stake
+                </h4>
+                <img
+                  src="/img/icons/stack.svg"
+                  className="object-contain justify-self-end h-12"
+                ></img>
+              </div>
+              <p className="text-sm w-64 mt-32 ">
+                Stakers play import roles at omitch. Stakers deposit their
+                stable coins into the protocal to back bets, in exchange stakers
+                earn apy and partake in protocols profit and loss
+              </p>
+              <div className="bg-[#2663FF] w-[80px] text-center rounded text-sm py-1 mt-5">
+                Stake
+              </div>
             </div>
-            <p className="text-sm text-white text-right  max-w-lg mt-1">
-              You choose to win big, you choose to play from varieties of games
-              provided by OmitchBet which includes: Slots, Cards, Crash,
-              Roullettes, Baccarat, and lots of others.
-            </p>
           </div>
         </div>
 
-        <div className="mt-32 relative container mx-auto max-w-screen-lg px-8">
-          <div className="font-heading uppercase text-white font-bold text-xl text-center tracking-wider">
+        <div className="mt-24 relative container mx-auto max-w-screen-lg px-8">
+          <h2 className="font-heading uppercase text-white font-bold text-xl text-center tracking-wider">
             How to get started
-          </div>
+          </h2>
           <div
             data-aos="fade-zoom-in"
             data-aos-easing="ease-in-back"
-            data-aos-delay="300"
+            data-aos-delay="100"
             data-aos-offset="0"
-            className="grid grid-cols-1 md:grid-cols-3 gap-y-5 md:gap-x-10 lg:gap-x-20 mt-6"
+            className="grid grid-cols-1 md:grid-cols-3 gap-y-5 md:gap-x-10 lg:gap-x-10 mt-6"
           >
             <div className="self-center">
-              <div className="bg-lightBlue text-center rounded-lg text-white text-sm py-4">
+              <div className="bg-[#75ADFF] text-center rounded-lg text-dark text-sm py-4">
                 Connect your wallet
               </div>
             </div>
             <div>
-              <div className="bg-orange text-center rounded-lg py-5 px-10 text-sm">
+              <div className="bg-[#75ADFF] text-center rounded-lg py-5 px-10 text-sm">
                 Deposit the $OMIT/USDC crypto assets at our platform's liquidity
                 pool.
               </div>
             </div>
             <div>
-              <div className="bg-yellow text-center rounded-lg py-5 px-10 text-sm">
+              <div className="bg-[#75ADFF] text-center rounded-lg py-5 px-10 text-sm">
                 Place bets and earn rewards for your passion with our great
                 odds.
               </div>
             </div>
           </div>
         </div>
-        <div className="mt-32 relative container mx-auto max-w-screen-lg px-8">
+
+        <div className="container mx-auto mt-16 text-dark rounded-lg">
+          <div
+            className="w-full h-[160px] md:h-[250px] lg:h-[420px] bg-cover md:bg-contain mx-auto bg-no-repeat"
+            style={{ backgroundImage: "url(/img/home/fighter.svg)" }}
+          >
+            <h2 className="font-heading uppercase font-bold text-xl md:text-2xl text-left tracking-wider pt-8 pl-4 md:pt-32 md:pl-12">
+              Our community /p2
+            </h2>
+            <p className="text-md w-60 md:w-80 pl-4  md:pl-12">
+              Join our community to play with other gamers on our P2P platform
+            </p>
+          </div>
+        </div>
+
+        <div className="mt-16 md:mt-24 relative container mx-auto max-w-screen-lg px-8">
           <div className="font-heading uppercase text-white font-bold text-xl text-center tracking-wider">
             Our Games
           </div>
-          <div className="grid  sm:grid-cols-1 md:grid-cols-4  mt-6  gap-x-10 gap-y-10">
+          <div className="grid  grid-cols-2 md:grid-cols-4  mt-6 gap-x-5 gap-y-5  md:gap-x-10 md:gap-y-10">
             <div data-aos="flip-left">
               <img alt="" src="/img/home/bigwin.svg"></img>
               <p className="text-white text-lg text-center mt-2">Slots</p>
@@ -157,7 +188,34 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="mt-32 relative container mx-auto max-w-screen-lg px-8">
+        <div className="mt-16  md:mt-32 relative container max-w-screen-lg mx-auto grid md:grid-cols-6 gap-y-4 px-8 overflow-x-hidden">
+          <img
+            alt=""
+            className="object-cover justify-self-center col-span-6 md:col-span-1 h-24 md:h-24 mx-auto"
+            src="/img/home/coins.png"
+            data-aos="fade-right"
+          ></img>
+
+          <div className="col-span-6 md:col-span-4 justify-self-center">
+            <div className="h-1 bg-white rounded mb-5"></div>
+            <div className="uppercase text-white text-center font-heading font-bold text-2xl tracking-wider max-w-lg">
+              THE CHOICE IS YOURS
+            </div>
+            <p className="text-sm text-white text-center max-w-md mt-1">
+              You choose to win big, you choose to play from varieties of games
+              provided by OmitchBet which includes: Slots, Cards, Crash,
+              Roullettes, Baccarat, and lots of others.
+            </p>
+          </div>
+          <img
+            alt=""
+            className="object-cover col-span-6 md:col-span-1 h-24 md:h-24 mx-auto"
+            src="/img/home/dice.png"
+            data-aos="fade-left"
+          ></img>
+        </div>
+
+        {/* <div className="mt-32 relative container mx-auto max-w-screen-lg px-8">
           <h2 className="font-heading uppercase text-white font-bold text-xl text-center tracking-wider">
             Our Roadmap
           </h2>
@@ -167,7 +225,7 @@ const Home = () => {
               Coming soon!!
             </span>
           </h1>
-        </div>
+        </div> */}
         <div className="bg-darKbluebg mt-16">
           <div className="pt-16 pb-8 relative container mx-auto max-w-screen-lg grid grid-cols-1 md:grid-cols-2 px-8">
             <div>
